96a8d3d2c3d36db4821ad64f24a24ec4baac1822
Matt S <XxLMM13xXgaming@gmail.com>
2017-02-22 20:56:44 -0500

    aaa

--- lua/pbusiness/config/pb_mysqlconfig.lua
+++ lua/pbusiness/config/pb_mysqlconfig.lua
@@ -1,4 +1,8 @@
 PBusinessMySQLConfig = {}
 local config = PBusinessMySQLConfig
 
+config.PBusinessMySQLHost = ""
+config.PBusinessMySQLUsername = ""
+config.PBusinessMySQLPassword = ""
+config.PBusinessMySQLDatabase = ""
 config.PBusinessMySQLPort = 3306
